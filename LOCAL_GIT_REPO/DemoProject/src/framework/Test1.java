7deac9a6345658bb9ba1375e454c7db3c3a652fb
spdpro5 <spdhamapurkar@yahoo.com>
2016-07-16 18:51:37 +0530

    remove unneccessary code

--- LOCAL_GIT_REPO/DemoProject/src/framework/Test1.java
+++ LOCAL_GIT_REPO/DemoProject/src/framework/Test1.java
@@ -4,8 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("updated changes");
-		System.out.println("add demo changes..");
-		System.out.println("changes from new project");
 
 	}
 }
