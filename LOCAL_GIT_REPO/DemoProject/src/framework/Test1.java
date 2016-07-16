260466be5b199c4bb4075f9b5b5aa318a3091c50
spdpro5 <spdhamapurkar@yahoo.com>
2016-07-16 17:02:42 +0530

    added changes from new wd

--- LOCAL_GIT_REPO/DemoProject/src/framework/Test1.java
+++ LOCAL_GIT_REPO/DemoProject/src/framework/Test1.java
@@ -1,9 +1,11 @@
 package framework;
 
 public class Test1 {
-public static void main(String[] args) {
-	System.out.println("Hello World");
-	System.out.println("updated changes");
-	System.out.println("add demo changes..");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("updated changes");
+		System.out.println("add demo changes..");
+		System.out.println("changes from new project");
+
+	}
 }
